59cb52b6ac034c8add5d162837a7f08fd7b66ddb
karen amaya <142056471+kjmaya@users.noreply.github.com>
2026-06-16 20:25:32 -0500

    feat: wrap app with HelmetProvider

--- medisxime-landing/src/main.tsx
+++ medisxime-landing/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
 
@@ -20,8 +21,10 @@ window.fetch = async (...args) => {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 )
